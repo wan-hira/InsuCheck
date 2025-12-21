7a79fb01910c9ded2c19189e258809f03ab5133b
tasye <tasyetasye24@gmail.com>
2025-12-21 10:20:28 +0100

    add navigation logic and button click listeners to DashboardActivity

--- app/src/main/java/com/example/insucheck/DashboardActivity.java
+++ app/src/main/java/com/example/insucheck/DashboardActivity.java
@@ -1,8 +1,11 @@
 package com.example.insucheck;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,7 +13,29 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_dashboard);
+
+
     }
 
+
+    // Btn actions
+    public void addEntry(View view) {
+        Intent intent = new Intent(this, AddEntryActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void history(View view) {
+        Intent intent = new Intent(this, HistoryActivity.class);
+        startActivity(intent);
+    }
+
+    public void settings(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
 
 }
